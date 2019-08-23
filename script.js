cf3d53ba5b074c15de46b9cc255403eea172f3da
Yaroslav <encored@i.ua>
2019-08-23 19:42:30 +0300

    after reinstall win10

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-let num = 20;
+let b = 142.21312312345;
 
-switch (true) {
-    case num < 30:
-        console.log('Nein');
-        break;
-    case num > 30:
-        console.log('YESS');
-        break;
-    default:
-        console.log('taoe');
-        break;
-}
-do{
-    ++num;
-    console.log (num);
-
-}while (num < 22);
+console.log(
+    b.toFixed(2)
+);
