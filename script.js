53e42d801a6ec47b93afcbfae9e25e92146901e7
Yaroslav <encored@i.ua>
2019-08-27 13:45:13 +0300

    31.08.19 20:27

--- script.js
+++ script.js
@@ -1,5 +1,64 @@
-let b = 142.21312312345;
+'use strict';
+let soldier = {
+    health: 400,
+    armor: 200,
+    size: 150,
+};
+let objectNew = {};
+Object.assign(objectNew, soldier);
+( soldier === objectNew) ? console.log(true) : console.log(false);
 
-console.log(
-    b.toFixed(2)
-);
+function check(param) {
+    for (let key in param) {
+                if (key == 'size') {
+            return (param[key]);
+        }  continue;
+    }
+}
+let a = check(soldier);
+if (a === undefined) {
+console.log('функция работает'); } else { console.log(a);}
+
+let schedule = {
+    '6:00': 'nothing',
+    '6:20': 'nothing',
+    '7:20': 'nothing',
+    '8:20': 'nothing',
+    '8:30': 'get up'
+};
+function isEmpty (param) {
+    for (let key in param) {
+        if (param[key] == 'get up') {
+        return false; } 
+    }
+}
+let b = isEmpty(schedule);
+console.log(b);
+
+let salaries = {
+John: 100,
+Ann: 160,
+Pete: 130,
+};
+let summ = 0;
+for (let key in salaries) {
+    summ += salaries[key];
+    
+}
+console.log(summ);
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: 'my menu'
+};
+function multiplyNumeric(param) {
+    for (let key in param) {
+        if (typeof(param[key]) == Number) {
+            param[key] *= 2;
+        } continue;
+    }
+}
+console.log(menu);
+multiplyNumeric(menu);
+console.log(menu);
---
+++ startDOM.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+let box = document.getElementById('box'),
+btn = document.getElementsByTagName('button'),
+circle = document.getElementsByClassName('circle'),
+queries = document.querySelectorAll('.heart'),
+firstQuery = document.querySelector('.heart');
+
+console.log(box);
+console.log(btn);
+console.log(circle);
+console.log(firstQuery);
+
+
+
+
+};
